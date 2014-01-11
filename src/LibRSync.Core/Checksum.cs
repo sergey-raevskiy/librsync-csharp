e55282eadc78bcc2dc0ea1875bed458340ea75a6
sergey-raevskiy <ccik@inbox.ru>
2014-01-10 21:04:05 -0800

    Introduced Checksum class.

---
+++ src/LibRSync.Core/Checksum.cs
@@ -0,0 +1,40 @@
+﻿namespace LibRSync.Core
+{
+    internal class Checksum
+    {
+        private const uint RS_CHAR_OFFSET = 31;
+
+        public static uint Weak(byte[] buf, int len)
+        {
+            var s1 = 0u;
+            var s2 = 0u;
+
+            int i;
+
+            for (i = 0; i < (len - 4); i += 4)
+            {
+                s2 += 4*(s1 + buf[i]) + (uint) 3*buf[i + 1] +
+                      (uint) 2*buf[i + 2] + buf[i + 3] + 10*RS_CHAR_OFFSET;
+
+                s1 += (uint) (buf[i + 0] + buf[i + 1] + buf[i + 2] + buf[i + 3] +
+                              4*RS_CHAR_OFFSET);
+            }
+
+            for (; i < len; i++)
+            {
+                s1 += (buf[i] + RS_CHAR_OFFSET);
+                s2 += s1;
+            }
+
+            return (s1 & 0xffff) + (s2 << 16);
+        }
+
+        public static byte[] Strong(byte[] buf, int len)
+        {
+            using (var hash = new Md4())
+            {
+                return hash.ComputeHash(buf, 0, len);
+            }
+        }
+    }
+}
--- src/LibRSync.Core/RDiff.cs
+++ src/LibRSync.Core/RDiff.cs
@@ -5,33 +5,7 @@ namespace LibRSync.Core
 {
     public class RDiff
     {
-        private const uint RS_CHAR_OFFSET = 31;
         private const int RS_SIG_MAGIC = 0x72730136;
-
-        private uint CalcWeakSum(byte[] buf, int len)
-        {
-            var s1 = 0u;
-            var s2 = 0u;
-
-            int i;
-
-            for (i = 0; i < (len - 4); i += 4)
-            {
-                s2 += 4*(s1 + buf[i]) + (uint) 3*buf[i + 1] +
-                      (uint) 2*buf[i + 2] + buf[i + 3] + 10*RS_CHAR_OFFSET;
-
-                s1 += (uint) (buf[i + 0] + buf[i + 1] + buf[i + 2] + buf[i + 3] +
-                              4*RS_CHAR_OFFSET);
-            }
-
-            for (; i < len; i++)
-            {
-                s1 += (buf[i] + RS_CHAR_OFFSET);
-                s2 += s1;
-            }
-
-            return (s1 & 0xffff) + (s2 << 16);
-        }
 
         public void GetSignature(Stream input, Stream signature)
         {
@@ -46,14 +20,11 @@ namespace LibRSync.Core
                 var len = input.Read(buf, 0, buf.Length);
                 if (len < 1) break;
 
-                var weak = CalcWeakSum(buf, len);
+                var weak = Checksum.Weak(buf, len);
                 NetInt.Write(signature, (int) weak);
 
-                using (HashAlgorithm hash = new Md4())
-                {
-                    var md4 = hash.ComputeHash(buf, 0, len);
-                    signature.Write(md4, 0, 8);
-                }
+                var strong = Checksum.Strong(buf, len);
+                signature.Write(strong, 0, 8);
             }
         }
     }
