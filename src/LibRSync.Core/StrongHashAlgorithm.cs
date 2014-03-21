cbd0882341be1c938ceab4a11fea028ee34c0e4b
sergey-raevskiy <ccik@inbox.ru>
2014-03-21 12:16:55 +0000

    Added GetAlgorithm() method.

--- src/LibRSync.Core/StrongHashAlgorithm.cs
+++ src/LibRSync.Core/StrongHashAlgorithm.cs
@@ -1,7 +1,25 @@
+using System.Collections.Generic;
+
 namespace LibRSync.Core
 {
     public class StrongHashAlgorithm
     {
         public static readonly IStrongHashAlgorithm Md4 = new Md4HashAlgorithm();
+
+        private static Dictionary<string, IStrongHashAlgorithm> algorithms
+            = new Dictionary<string, IStrongHashAlgorithm>
+            {
+                {"md4", Md4}
+            };
+
+        public static IStrongHashAlgorithm GetAlgorithm(string name)
+        {
+            IStrongHashAlgorithm alg;
+
+            if (algorithms.TryGetValue(name, out alg))
+                return alg;
+            else
+                return null;
+        }
     }
 }
---
+++ src/LibRSync.Tests/HashAlgorithmTest.cs
@@ -0,0 +1,16 @@
+﻿using LibRSync.Core;
+using NUnit.Framework;
+
+namespace LibRSync.Tests
+{
+    [TestFixture]
+    public class HashAlgorithmTest
+    {
+        [Test]
+        public void GetAlgorithTest()
+        {
+            Assert.AreSame(StrongHashAlgorithm.Md4, StrongHashAlgorithm.GetAlgorithm("md4"));
+            Assert.IsNull(StrongHashAlgorithm.GetAlgorithm("super-srypto"));
+        }
+    }
+}
