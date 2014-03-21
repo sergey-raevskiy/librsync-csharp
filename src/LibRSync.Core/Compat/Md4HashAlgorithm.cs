67ccb7a0d7f7819dcef04a33f17f975681a071f0
sergey-raevskiy <ccik@inbox.ru>
2014-03-21 12:18:55 +0000

    Md4HashAlgorithm moved to Compat.

--- src/LibRSync.Core/Compat/Md4HashAlgorithm.cs
+++ src/LibRSync.Core/Compat/Md4HashAlgorithm.cs
@@ -2,7 +2,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Security.Cryptography;
 
-namespace LibRSync.Core
+namespace LibRSync.Core.Compat
 {
     internal class Md4HashAlgorithm : IStrongHashAlgorithm
     {
--- src/LibRSync.Core/StrongHashAlgorithm.cs
+++ src/LibRSync.Core/StrongHashAlgorithm.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using LibRSync.Core.Compat;
 
 namespace LibRSync.Core
 {
