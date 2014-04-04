89f0549343c018b190519d171f8fad29fe5e7dbb
sergey-raevskiy <ccik@inbox.ru>
2014-04-04 15:41:11 +0000

    Added some basic http functionality.

---
+++ src/LibRSync.Tests/HttpTests.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.IO;
+using System.Net.Http;
+using Nancy;
+using Nancy.Hosting.Self;
+using NUnit.Framework;
+
+namespace LibRSync.Tests
+{
+    [TestFixture]
+    public class HttpTests : TestBase
+    {
+        private NancyHost host;
+        public HttpClient Client { get; set; }
+
+        private class PathProvider : IRootPathProvider
+        {
+            private string path;
+
+            public PathProvider(string path)
+            {
+                this.path = path;
+            }
+
+            public string GetRootPath()
+            {
+                return path;
+            }
+        }
+
+        private class TestBootstrapper : DefaultNancyBootstrapper
+        {
+            private PathProvider provider;
+
+            public TestBootstrapper(string rootPath)
+            {
+                this.provider = new PathProvider(rootPath);
+            }
+
+            protected override IRootPathProvider RootPathProvider
+            {
+                get { return provider; }
+            }
+        }
+
+        [SetUp]
+        public void SetUp()
+        {
+            var cfg = new HostConfiguration
+            {
+                UrlReservations = new UrlReservations
+                {
+                    CreateAutomatically = true,
+                    User = "Everyone"
+                }
+            };
+
+            var baseAddress = new Uri("http://localhost:12345");
+
+            host = new NancyHost(new TestBootstrapper(Directory.GetCurrentDirectory()), cfg, baseAddress);
+            host.Start();
+
+            Client = new HttpClient
+            {
+                BaseAddress = baseAddress
+            };
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            Client.Dispose();
+            host.Dispose();
+        }
+
+        private static readonly HttpMethod Patch = new HttpMethod("PATCH");
+
+        [TestCase("delta.input.01.delta", "delta.input.01.expect")]
+        [TestCase("delta.input.02.delta", "delta.input.02.expect")]
+        [TestCase("delta.input.03.delta", "delta.input.03.expect")]
+        public void PatchTest(string deltaName, string expectedName)
+        {
+            File.WriteAllBytes("file.txt", new byte[0]);
+
+            var request = new HttpRequestMessage(Patch, "/res/file.txt");
+            request.Content = new StreamContent(GetTestDataStream(deltaName));
+
+            var res = Client.SendAsync(request).Result;
+            Assert.AreEqual(System.Net.HttpStatusCode.NoContent, res.StatusCode);
+
+            Assert.AreEqual(
+                StreamToArray(GetTestDataStream(expectedName)),
+                File.ReadAllBytes("file.txt"));
+        }
+    }
+}
---
+++ src/SyncHttpServer/SyncModule.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.ComponentModel;
+using System.IO;
+using System.Runtime.InteropServices;
+using LibRSync.Core;
+using Nancy;
+
+namespace SyncHttpServer
+{
+    public class SyncModule : NancyModule
+    {
+        private readonly IRootPathProvider pathProvider;
+
+        public SyncModule(IRootPathProvider pathProvider)
+        {
+            this.pathProvider = pathProvider;
+
+            Patch["/res/{path*}"] = PatchFile;
+        }
+
+        private FileStream OpenTempFile(string filePath, out string tempPath)
+        {
+            var dir = Path.GetDirectoryName(filePath);
+            tempPath = Path.Combine(dir, Guid.NewGuid().ToString("n"));
+
+            return File.Open(tempPath, FileMode.CreateNew, FileAccess.Write, FileShare.None);
+        }
+
+        [Flags]
+        internal enum MoveFileFlags
+        {
+            None = 0,
+            ReplaceExisting = 1,
+            CopyAllowed = 2,
+            DelayUntilReboot = 4,
+            WriteThrough = 8,
+            CreateHardlink = 16,
+            FailIfNotTrackable = 32,
+        }
+
+        [DllImport("kernel32.dll", SetLastError = true, CharSet = CharSet.Unicode)]
+        private static extern bool MoveFileEx(
+            string lpExistingFileName,
+            string lpNewFileName,
+            MoveFileFlags dwFlags);
+
+        private object PatchFile(dynamic p)
+        {
+            var path = Path.Combine(pathProvider.GetRootPath(), p.path);
+            string tempPath;
+
+            using (var file = File.Open(path, FileMode.Open, FileAccess.Read, FileShare.Delete))
+            {
+                using (var tempFile = OpenTempFile(path, out tempPath))
+                {
+                    var patchJob = new PatchJob(file, Request.Body, tempFile);
+                    patchJob.Run();
+                }
+            }
+
+            if (!MoveFileEx(tempPath, path, MoveFileFlags.ReplaceExisting))
+                throw new Win32Exception();
+
+            return 204;
+        }
+    }
+}
