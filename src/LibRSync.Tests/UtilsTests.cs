9a0fcb9683e6d340c6a885d3deb8ee3aa1e0ce46
sergey-raevskiy <ccik@inbox.ru>
2014-08-07 15:24:10 +0400

    Added tests for Utils.GetIntLen().

---
+++ src/LibRSync.Tests/UtilsTests.cs
@@ -0,0 +1,23 @@
+using LibRSync.Core;
+using NUnit.Framework;
+
+namespace LibRSync.Tests
+{
+    [TestFixture]
+    public class UtilsTests
+    {
+        [TestCase(1L, 1)]
+        [TestCase(0xffff, 2)]
+        [TestCase(0x01ff, 2)]
+        [TestCase(0xffffff, 4)]
+        [TestCase(0x01ffff, 4)]
+        [TestCase(0x1ffffff, 4)]
+        [TestCase(0xffffabcd, 4)]
+        [TestCase(0x1ffffabcd, 8)]
+        [TestCase(0xabcdabcddeadeadL, 8)]
+        public void IntLenTest(long i, int expected)
+        {
+            Assert.AreEqual(expected, Utils.GetIntLen(i));
+        }
+    }
+}
